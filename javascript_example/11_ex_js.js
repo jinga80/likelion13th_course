c0b279ceb64d19a2f40f5deadbb33fb330ab2259
jinga80 <powernt@naver.com>
2021-09-07 15:04:28 +0900

    자바스크립트 실습 정리
    자바스크립트 실습 정리파일
    크롬 크롤링 시작

---
+++ javascript_example/11_ex_js.js
@@ -0,0 +1,6 @@
+
+function change_p() {
+    document.getElementById("demo").innerHTML = "내용이 성공적으로 변경됬습니다."
+    }
+
+    
---
+++ javascript_example/test.js
@@ -0,0 +1,6 @@
+
+function cal() {
+    var txt = "I can eat bananas all day";
+    var x = txt.slice(0,6);
+    alert(x);
+    }
